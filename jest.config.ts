14d8c7ae25768625878182074b45a2761564339f
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-05-28 21:34:25 +0200

    fix: jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,17 @@
-const config = {
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
   clearMocks: true,
   collectCoverage: false,
   errorOnDeprecated: true,
   moduleDirectories: ['node_modules', '<rootDir>'],
-  preset: 'ts-jest/presets/js-with-ts',
   restoreMocks: true,
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup/jestDomExpects.ts'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
-  globals: {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
 };
 
-export default config;
+export default createJestConfig(customJestConfig);
