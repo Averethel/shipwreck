20ac423bedc921569d2e4c18fab48deb3f159cea
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-05-28 21:39:54 +0200

    chore: module mapping

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,14 @@ const customJestConfig = {
   restoreMocks: true,
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '^@components/(.*)$': '<rootDir>/components/$1',
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '^@pages/(.*)$': '<rootDir>/pages/$1',
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+  },
 };
 
 export default createJestConfig(customJestConfig);
